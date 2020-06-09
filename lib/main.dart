7dda38e47a8130071dd40c5b60a921a9348d0451
SAMI SHEHZAD <samishehzad1106@gmail.com>
2020-06-09 20:37:15 +0500

    tap on event to show its location on a map

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:fooden/screens/launch_screen.dart';
 import 'package:fooden/screens/login_screen.dart';
 import 'package:fooden/screens/payment_screen.dart';
 import 'package:fooden/screens/registration_screen.dart';
+import 'package:fooden/screens/volunteer_screen.dart';
 import 'package:fooden/screens/welcome_screen.dart';
 
 void main() => runApp(MyApp());
@@ -26,6 +27,7 @@ class MyApp extends StatelessWidget {
         'home_screen': (context) => HomeScreen(),
         'payment_screen': (context) => PaymentScreen(),
         'detail_form_screen': (context) => DetailForm(),
+        'volunteer_screen': (context) => MyMap(),
       },
      // home: LaunchScreen(),
     );
--- lib/screens/event_screen.dart
+++ lib/screens/event_screen.dart
@@ -5,6 +5,7 @@ import 'package:fooden/models/events.dart';
 import 'package:fooden/screens/add_event_screen.dart';
 import 'dart:math' as math;
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fooden/screens/volunteer_screen.dart';
 
 class EventScreen extends StatefulWidget {
   @override
@@ -104,118 +105,123 @@ ListView getListViewBuilder(List<Event> events) {
       final Event event = events[index];
       return Padding(
         padding: EdgeInsets.all(5.0),
-        child: Container(
-          decoration: kEventBoxDecoration,
-          child: Dismissible(
-            onDismissed: (direction) {
-              // if (direction == DismissDirection.endToStart) {
-              //   setState(() {
-              //     events.removeAt(index);
-              //   });
-              // }
-            },
-            direction: DismissDirection.endToStart,
-            background: Container(
-              alignment: Alignment.centerRight,
-              padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-              decoration: kEventBoxDecoration,
-              child: Transform.rotate(
-                angle: 45 * math.pi / 180,
-                child: Container(
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    border: Border.all(width: 2.0, color: Colors.white),
-                  ),
-                  child: Icon(
-                    Icons.add,
-                    color: Colors.white,
+        child: GestureDetector(
+          onTap: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) => MyMap(location: event.location,)));
+          },
+          child: Container(
+            decoration: kEventBoxDecoration,
+            child: Dismissible(
+              onDismissed: (direction) {
+                // if (direction == DismissDirection.endToStart) {
+                //   setState(() {
+                //     events.removeAt(index);
+                //   });
+                // }
+              },
+              direction: DismissDirection.endToStart,
+              background: Container(
+                alignment: Alignment.centerRight,
+                padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                decoration: kEventBoxDecoration,
+                child: Transform.rotate(
+                  angle: 45 * math.pi / 180,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(width: 2.0, color: Colors.white),
+                    ),
+                    child: Icon(
+                      Icons.add,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
               ),
-            ),
-            key: UniqueKey(),
-            child: Container(
-              //margin: EdgeInsets.all(5.0),
-              padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-              decoration: kEventBoxDecoration.copyWith(
-                color: event.isHandled
-                    ? Color(0xFFebffe8)
-                    : Color(0xFFFFEFEE), //Color(0xFFebffe8)
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey,
-                    offset: Offset(0.0, 2.0), //(x,y)
-                    blurRadius: 6.0,
-                  ),
-                ],
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
+              key: UniqueKey(),
+              child: Container(
+                //margin: EdgeInsets.all(5.0),
+                padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                decoration: kEventBoxDecoration.copyWith(
+                  color: event.isHandled
+                      ? Color(0xFFebffe8)
+                      : Color(0xFFFFEFEE), //Color(0xFFebffe8)
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      offset: Offset(0.0, 2.0), //(x,y)
+                      blurRadius: 6.0,
+                    ),
+                  ],
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          "Task # " + (index + 1).toString(),
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.grey,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 5.0),
+                        Text(
+                          event.location,
+                          style: TextStyle(
+                            fontSize: 15.0,
+                            color: event.isHandled ? Colors.green :Colors.red,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.7,
+                          child: Text(
+                            event.description,
+                            style: TextStyle(
+                                fontSize: 20.0,
+                                color: Colors.blueGrey,
+                                fontWeight: FontWeight.w600),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(children: <Widget>[
                       Text(
-                        "Task # " + (index + 1).toString(),
+                        event.amount.toString() + 'kg',
                         style: TextStyle(
-                          fontSize: 20.0,
+                          fontSize: 15.0,
                           color: Colors.grey,
                           fontWeight: FontWeight.bold,
                         ),
                       ),
                       SizedBox(height: 5.0),
-                      Text(
-                        event.location,
-                        style: TextStyle(
-                          fontSize: 15.0,
-                          color: event.isHandled ? Colors.green :Colors.red,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Container(
-                        width: MediaQuery.of(context).size.width * 0.7,
-                        child: Text(
-                          event.description,
-                          style: TextStyle(
-                              fontSize: 20.0,
-                              color: Colors.blueGrey,
-                              fontWeight: FontWeight.w600),
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Column(children: <Widget>[
-                    Text(
-                      event.amount.toString() + 'kg',
-                      style: TextStyle(
-                        fontSize: 15.0,
-                        color: Colors.grey,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(height: 5.0),
-                    event.isHandled
-                        ? Container(
-                            width: 40.0,
-                            height: 20.0,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(30.0),
-                                color: Colors.green),
-                            alignment: Alignment.center,
-                            child: Text(
-                              "Done",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 12.0,
-                                fontWeight: FontWeight.bold,
+                      event.isHandled
+                          ? Container(
+                              width: 40.0,
+                              height: 20.0,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(30.0),
+                                  color: Colors.green),
+                              alignment: Alignment.center,
+                              child: Text(
+                                "Done",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 12.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
-                            ),
-                          )
-                        : SizedBox.shrink()
-                  ])
-                ],
+                            )
+                          : SizedBox.shrink()
+                    ])
+                  ],
+                ),
               ),
             ),
           ),
